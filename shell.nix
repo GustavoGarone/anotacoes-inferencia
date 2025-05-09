9e093e539c802cc4dce5c965abbdba562d64ea46
Minze <gustavogarone@protonmail.com>
2025-05-09 09:38:03 -0300

    aula 090525

--- shell.nix
+++ shell.nix
@@ -16,6 +16,7 @@ in
       pypkgs.numpy
       pypkgs.ipython
       pypkgs.jupyter
+      pypkgs.jupyter-cache
       pypkgs.matplotlib
     ]))
     my-r
@@ -24,27 +25,6 @@ in
     julia
     curl
   ]);
-
-  # Discouraged, find alternative solution
-  # NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
-  #   gcc  # ...
-  #   gfortran
-  #   stdenv
-  #   openspecfun
-  #   curl
-  #   proj
-  #   sqlite
-  #   geos
-  #   libssh2
-  # ];
-  # NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
-  # shellHook = ''
-  #   # Set R_LIBS_USER to an empty value to avoid unwanted user-specific libraries
-  #   export LD_LIBRARY_PATH="${my-r}/lib/R/lib:$LD_LIBRARY_PATH"
-  #   export R_LIBS_USER=""
-  #   export R_LIBS_SITE=$(R RHOME)/library
-  #   export R_HOME=$(R RHOME)
-  # '';
   runScript = "zsh";
 }).env
 
